75fc563542d29dbc08ecce5253133d9dbc27f4c2
vepifanio <vepifanio1@gmail.com>
2023-09-26 11:17:56 -0300

    feat: Register controller/use case | Users repository/Prisma repository added

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,21 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    return prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+}
