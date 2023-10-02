33ddd49a3aaf4548af2cfd47fc2423033db4a7b5
vepifanio <vepifanio1@gmail.com>
2023-10-02 19:06:26 -0300

    feat: Get user metrics use case | tests

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetUserMetricsUseCase } from './get-user-metrics'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: GetUserMetricsUseCase
+
+describe('Get user metrics use case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('Should be able to get check-ins count from metrics', async () => {
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-02',
+    })
+
+    const { checkInsCount } = await sut.execute({
+      userId: 'user-01',
+    })
+
+    expect(checkInsCount).toEqual(2)
+  })
+})
