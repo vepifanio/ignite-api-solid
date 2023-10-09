0c2000f5100f1dee2a7bcac32e49e292145f252a
vepifanio <vepifanio1@gmail.com>
2023-10-09 10:15:45 -0300

    chore: Prisma test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Environment } from 'vitest'
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    console.log('Setup')
+
+    return {
+      async teardown() {
+        console.log('Teardown')
+      },
+    }
+  },
+  transformMode: 'ssr',
+}
